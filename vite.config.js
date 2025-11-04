5477c5390494454b3fa761810ebd5054d0aa78aa
Bishakha Kumari <bishakha005kr@gmail.com>
2025-11-04 19:58:40 +0530

    Added vite plugin react and fixed build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,        // Allows access from mobile devices
-    port: 5173         // You can change this if needed
-  }
-});
+  build: {
+    outDir: 'dist'
+  },
+})
