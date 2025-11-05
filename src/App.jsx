af7ae4f615505b5ff5340af770612427bc903aaa
Bishakha Kumari <bishakha005kr@gmail.com>
2025-11-05 13:21:28 +0530

    Fixed: Added react-icons dependency for Vercel build

--- src/App.jsx
+++ src/App.jsx
@@ -357,7 +357,8 @@ onClick={() => {
   </div>
 </section>
 
-<footer className="bg-gray-50 border-t border-gray-200 text-gray-700 pt-10 pb-6">
+<footer className="bg-pink-50 border-t border-gray-200 text-gray-700 pt-10 pb-6">
+
   <div className="max-w-6xl mx-auto px-4 grid grid-cols-1 md:grid-cols-3 gap-8 text-center md:text-left">
     
     {/* Shop Categories */}
