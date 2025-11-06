0b7aebca3b38b1d73f76b26a97e4daa18d98c61e
Bishakha Kumari <bishakha005kr@gmail.com>
2025-11-07 05:04:14 +0530

    Fixed: Added react-icons dependency for Vercel build

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import CategoryPage from './pages/CategoryPage.jsx'; // make sure this file exis
 import './index.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import FloatingWhatsApp from "./components/FloatingWhatsApp.jsx";
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
