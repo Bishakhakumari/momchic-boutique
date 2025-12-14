82ea096ac7cc2256b90204ac1e099a0e9f95c6d4
Bishakha Kumari <bishakha005kr@gmail.com>
2025-12-14 23:05:33 +0530

    Fixed: Added react-icons dependency for Vercel build

--- src/components/Herobanner.jsx
+++ src/components/Herobanner.jsx
@@ -2,13 +2,13 @@ import { useEffect, useState, useRef } from "react";
 
 export default function HeroBanner() {
   const bannerImages = [
-    "https://res.cloudinary.com/dm5ksdp5o/image/upload/v1762614252/mohan-cinema-momchic-boutiuqe-daltonganj-bus-stand.jpg",
-    "https://res.cloudinary.com/dm5ksdp5o/image/upload/v1762607877/mohan-cinema-momchic-boutiuqe-daltonganj-grand-opening-mayor-aruna-shankar.jpg",
-    "https://res.cloudinary.com/dm5ksdp5o/image/upload/v1762707346/momchic-boutique-first-mayor-aruna-shankar-daltonganj.jpg",
-    "https://res.cloudinary.com/dm5ksdp5o/image/upload/v1762607878/mohan-cinema-momchic-boutiuqe-daltonganj-bus-stand-game-zone-latest.jpg",
-    "https://res.cloudinary.com/dm5ksdp5o/image/upload/v1762607877/mohan-cinema-momchic-boutiuqe-daltonganj-latest-news-mayor-aruna-shankar-opening.jpg",
-    "https://res.cloudinary.com/dm5ksdp5o/image/upload/v1762607879/mohan-cinema-momchic-boutiuqe-daltonganj-bus-stand-latest.jpg",
-    "https://res.cloudinary.com/dm5ksdp5o/image/upload/v1762631933/momchic-boutique-first-mayor-aruna-shankar-daltonganj-latest.jpg",
+    "https://ik.imagekit.io/momchicboutique/Banner/mohan-cinema-momchic-boutiuqe-daltonganj-bus-stand.jpg?updatedAt=1765733210488",
+    "https://ik.imagekit.io/momchicboutique/Banner/https:__ik.imamohan-cinema-momchic-boutiuqe-daltonganj-bus-stand-1.jpg?updatedAt=1765733312639",
+    "https://ik.imagekit.io/momchicboutique/Banner/mohan-cinema-momchic-boutiuqe-daltonganj-bus-stand-2.jpg?updatedAt=1765733391917",
+    
+    "https://ik.imagekit.io/momchicboutique/Banner/mohan-cinema-momchic-boutiuqe-daltonganj-latest-news-mayor-aruna-shankar-opening-1.jpg?updatedAt=1765733503595",
+    "https://ik.imagekit.io/momchicboutique/Banner/mohan-cinema-momchic-boutiuqe-daltonganj-latest-news-mayor-aruna-shankar-opening-3.jpg?updatedAt=1765733590889",
+    "https://ik.imagekit.io/momchicboutique/Banner/mohan-cinema-momchic-boutiuqe-daltonganj-latest-news-mayor-aruna-shankar-opening-4.jpg?updatedAt=1765733641834",
   ];
 
   const [currentImage, setCurrentImage] = useState(0);
